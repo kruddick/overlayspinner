4dac925c7fa6ac00c9ea6b610d0fa79655b3af52
Kent Ruddick <kruddick@gmail.com>
2023-01-24 06:47:19 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,88 @@
+<script>
+	import { onMount } from "svelte";
+
+    let show = false;
+
+    function eventCallback(messageName, messageBody) {
+		console.log("eventCallback: " + messageName);
+		if(messageName === "spinner-show") {
+			show = true;
+		} else if(messageName === "spinner-hide") {
+			show = false;			
+		}		
+    }
+
+    onMount(() => {
+        console.log("onMount");
+        if(window.messageBus.register) {
+            window.messageBus.register(eventCallback);
+            console.log("onMount: registered")
+        }
+    });
+</script>
+
+{#if show}
+  <div class="overlay">
+    <div class="spinner">
+      <div class="bounce1"></div>
+      <div class="bounce2"></div>
+      <div class="bounce3"></div>
+    </div>
+  </div>
+{/if}
+
+<style>
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .spinner {
+    width: 50px;
+    height: 40px;
+    text-align: center;
+    font-size: 10px;
+  }
+
+  .spinner > div {
+    background-color: #333;
+    height: 100%;
+    width: 6px;
+    display: inline-block;
+    -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+    animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  }
+
+  .spinner .bounce1 {
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+
+  .spinner .bounce2 {
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+  }
+
+  @-webkit-keyframes sk-bouncedelay {
+    0%, 80%, 100% { -webkit-transform: scale(0) }
+    40% { -webkit-transform: scale(1.0) }
+  }
+
+  @keyframes sk-bouncedelay {
+    0%, 80%, 100% { 
+      -webkit-transform: scale(0);
+      transform: scale(0);
+    } 40% { 
+      -webkit-transform: scale(1.0);
+      transform: scale(1.0);
+    }
+  }
+</style>
